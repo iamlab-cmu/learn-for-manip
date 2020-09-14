fc17484e8122b98327dfb1d32be58e4c830d26fe
Timothy Lee <tel1@andrew.cmu.edu>
2020-09-14 07:03:17 -0400

    Adding NUC install script

---
+++ nuc_install/install_nuc_locobot.sh
@@ -0,0 +1,14 @@
+# Useful utils
+sudo apt install xclip
+sudo apt install htop
+sudo apt install gparted
+
+# Remove Amazon shortcut
+sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/Amazon.user.js
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/manifest.json
+
+# Other useful things I haven't automated yet
+# changing bashrc to color terminal
+# changing bashrc history to 20000
+
