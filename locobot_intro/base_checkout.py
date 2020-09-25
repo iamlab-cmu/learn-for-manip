27509aac26610f52529323ddb78a5544bde9a68e
Timothy Lee <tel1@andrew.cmu.edu>
2020-09-25 10:17:11 -0400

    Updated base_checkout script

--- locobot_intro/base_checkout.py
+++ locobot_intro/base_checkout.py
@@ -1,20 +1,49 @@
+# Use this command to launch the control script:
+# roslaunch locobot_control main.launch use_arm:=true use_base:=true base:=create
+
 # Always use the "Locobot-Lite" configuration
+import sys
 import time
+
 import numpy as np
+
 from pyrobot import Robot
 
-base_config_dict={'base_controller': 'movebase'}
-robot = Robot('locobot_lite', base_config=base_config_dict)
+def base_checkout(input_args):
 
-# Reset
-robot.arm.go_home()
+    # Warn the user
+    user_response = raw_input("The robot base is about to move. Is the area clear? (Y/n): ")
+    if user_response and user_response.lower() != 'y':
+        print("Invalid response. Exiting.")
+        return False
 
-# Turn in place
-target_position = [0.0, 0.0, 1.5707] # rotate on-spot by 90 degrees
-robot.base.go_to_relative(target_position, smooth=False, close_loop=True)
-time.sleep(1)
+    # This controller may not be the best for all applications
+    # Please revisit the choice of base controller for other applications
+    base_config_dict={'base_controller': 'movebase'}
+    robot = Robot('locobot_lite', base_config=base_config_dict)
 
-# Move a bit
-target_position = [1.0, 1.0, 0.0] 
-robot.base.go_to_relative(target_position, smooth=False, close_loop=True)
-time.sleep(1)
+    # Reset
+    print('Setting arm to home.')
+    robot.arm.go_home()
+
+    # Turn in place
+    target_position = [0., 0., 1.5707] # rotate on-spot by 90 degrees
+    print('Turning in place.')
+    robot.base.go_to_relative(
+        target_position, smooth=False, close_loop=True
+    )
+    time.sleep(1)
+
+    # Move a bit
+    target_position = [0.5, 0., 0.]
+    print('Moving base forward.')
+    robot.base.go_to_relative(
+        target_position, smooth=False, close_loop=True
+    )
+    time.sleep(1)
+
+    odom_state = robot.base.get_state('odom')
+    print('Current position from odometry [x/y/yaw]: {}'.format(odom_state))
+
+if __name__ == '__main__':
+    base_checkout(sys.argv[1:])
