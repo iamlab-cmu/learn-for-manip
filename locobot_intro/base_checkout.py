979ebc76800a973387269539e633100ef7438021
Timothy Lee <tel1@andrew.cmu.edu>
2020-10-02 13:04:51 -0400

    Added base rotation and reduced forward movement.

--- locobot_intro/base_checkout.py
+++ locobot_intro/base_checkout.py
@@ -1,5 +1,5 @@
 # Use this command to launch the control script:
-# roslaunch locobot_control main.launch use_arm:=true use_base:=true base:=create
+# roslaunch locobot_control main.launch use_base:=true base:=create
 
 # Always use the "Locobot-Lite" configuration
 import sys
@@ -22,10 +22,6 @@ def base_checkout(input_args):
     base_config_dict={'base_controller': 'movebase'}
     robot = Robot('locobot_lite', base_config=base_config_dict)
 
-    # Reset
-    print('Setting arm to home.')
-    robot.arm.go_home()
-
     # Turn in place
     target_position = [0., 0., 1.5707] # rotate on-spot by 90 degrees
     print('Turning in place.')
@@ -34,8 +30,24 @@ def base_checkout(input_args):
     )
     time.sleep(1)
 
+    odom_state = robot.base.get_state('odom')
+    print('Current position from odometry [x/y/yaw]: {}'.format(odom_state))
+    time.sleep(1)
+
+    # Turn in place
+    target_position = [0., 0., -1.5707] # rotate on-spot by 90 degrees
+    print('Turning in place.')
+    robot.base.go_to_relative(
+        target_position, smooth=False, close_loop=True
+    )
+    time.sleep(1)
+
+    odom_state = robot.base.get_state('odom')
+    print('Current position from odometry [x/y/yaw]: {}'.format(odom_state))
+    time.sleep(1)
+
     # Move a bit
-    target_position = [0.5, 0., 0.]
+    target_position = [0.25, 0., 0.]
     print('Moving base forward.')
     robot.base.go_to_relative(
         target_position, smooth=False, close_loop=True
@@ -44,6 +56,7 @@ def base_checkout(input_args):
 
     odom_state = robot.base.get_state('odom')
     print('Current position from odometry [x/y/yaw]: {}'.format(odom_state))
+    time.sleep(1)
 
 if __name__ == '__main__':
     base_checkout(sys.argv[1:])
